b83413562f1529c5498911e2d3482c8aa785542f
vivcogit <vivcogit@gmail.com>
2020-01-24 22:51:43 +0700

    fix: Fixed paths

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,6 @@
 import App from 'next/app';
 
-import Page from '../components/page';
+import Page from '../components/Page';
 import apiProvider from '../providers/api';
 
 class MyApp extends App {
